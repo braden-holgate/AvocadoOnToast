1bf4594e65b5c5eeda3c4e02ffb5e28945a140db
Louisa Thomas <louisathomas@Karens-MBP.localdomain>
2022-05-05 11:51:09 +1200

    empty components

---
+++ client/components/TimeCostTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TimeCostTable
+ () {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default TimeCostTable
+
---
+++ client/components/YearsToRetire.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function YearsToRetire () {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default YearsToRetire
