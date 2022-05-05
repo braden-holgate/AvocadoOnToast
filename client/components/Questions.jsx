5e75e807f68b768f8602472ffc97584be4844bd8
muer-zhang <98420889+muer-zhang@users.noreply.github.com>
2022-05-05 22:45:50 +1200

    add placeholder text that clears on click for input fields

--- client/components/Questions.jsx
+++ client/components/Questions.jsx
@@ -6,32 +6,33 @@ function Questions () {
   return (
     <>
 
-      <div style={{whiteSpace: 'nowrap'}}>
+      <div style={{whiteSpace: 'nowrap', marginBottom: '25px', marginTop: '25px'}}>
             <strong><label className="mr-2">What is your income?</label></strong>
-            <EditText name="income" type="text" style={{width: '200px', margin: '8px', padding: '5px'}} defaultValue="Enter your income here" inline/>
+            <EditText name="income" type="text" style={{width: '200px', padding: '5px', border: '1px solid #ccc'}} placeholder="0.00" defaultValue='' inline/>
+
           </div>
       
-      <div style={{whiteSpace: 'nowrap'}}>
+      <div style={{whiteSpace: 'nowrap', marginBottom: '25px'}}>
             <strong><label className="mr-2">How much do you estimate you save?</label></strong>
-            <EditText name="average-savings" type="text" style={{width: '200px'}} defaultValue="Enter how much you think you save here" inline/>
+            <EditText name="average-savings" type="text" style={{width: '200px', padding: '5px', border: '2px solid #ccc'}} placeholder="Enter how much you think you save here" defaultValue='' inline/>
           </div>
 
-      <div style={{whiteSpace: 'nowrap'}}>
+      <div style={{whiteSpace: 'nowrap', marginBottom: '25px'}}>
         <strong><label className="mr-2">How much do you currently have saved?</label></strong>
-        <EditText name="current-savings" type="text" style={{width: '200px'}} defaultValue="Enter how much you already have saved here" inline/>
+        <EditText name="current-savings" type="text" style={{width: '200px', padding: '5px', border: '2px solid #ccc'}}placeholder='Enter how much you already have saved here' defaultValue='' inline/>
       </div>
 
       
       {/* single numerical input - we probably need to do Number('user input') */}
       --------------
  {/* //LOU - MAKE SURE TO PREVENT NEGATIVE NUMBERS */}
-<div style={{whiteSpace: 'nowrap'}}>
+<div style={{whiteSpace: 'nowrap', marginBottom: '25px'}}>
             <strong><label className="mr-2">How often do you buy coffee per week?</label></strong>
-            <EditText name="coffees" type="number" style={{width: '200px'}} defaultValue="0" inline/>
+            <EditText name="coffees" type="number" style={{width: '200px', padding: '5px', border: '2px solid #ccc'}} placeholder='Enter your average weekly coffee count' defaultValue="" inline/>
           </div>
-          <div style={{whiteSpace: 'nowrap'}}>
+          <div style={{whiteSpace: 'nowrap', marginBottom: '25px'}}>
             <strong><label className="mr-2">How often do you buy lunch/dinner/takeaways per week?</label></strong>
-            <EditText name="food" type="number" style={{width: '200px'}} defaultValue="0" inline/>
+            <EditText name="food" type="number" style={{width: '200px', padding: '5px', border: '2px solid #ccc'}} placeholder='Enter your weekly takeout count' defaultValue='' inline/>
           </div>
     
    
