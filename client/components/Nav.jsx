20f39cca5caaaab51c2827eb5134a095b69fb9fe
Louisa Thomas <louisathomas@Karens-MBP.localdomain>
2022-05-05 12:33:45 +1200

    add basic content to components

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function Nav () {
   return (
     <>
- 
+ <div>Login</div>
+ <div>Register</div> 
+ <div>Logout</div> 
+ {/* //We will only ever display a max of 2 of these at a time. If user not logged in, we show Login and Register. If they are logged in, we show Logout. */}
     
     </>
   )
--- client/components/YearsToRetire.jsx
+++ client/components/YearsToRetire.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function YearsToRetire () {
   return (
     <>
-    
+    <h2>This is how many years you currently have until you can retire:</h2>
+    <div>00</div>
     </>
   )
 }
