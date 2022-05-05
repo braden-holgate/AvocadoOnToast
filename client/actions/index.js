3664c0d8b01791cc4cfd0c7c15364b4a17eb8bee
Kate Blincoe <95556689+kate-blincoe@users.noreply.github.com>
2022-05-05 16:49:59 +1200

    set up db server route and start reducers etc

---
+++ client/actions/index.js
@@ -0,0 +1,38 @@
+import request from 'superagent'
+
+export const RECIEVE_COSTS = 'RECEIVE_COSTS'
+export const LOAD_FREQUENCY = 'LOAD_FREQUENCY'
+export const LOAD_FINANCIALS = 'LOAD_FINANCIALS'
+export const SHOW_ERROR = 'SHOW_ERROR'
+
+//ACTION CREATORS
+export function recieveCosts(costs) {
+  return {
+    type: RECIEVE_COSTS,
+    cost,
+  }
+}
+
+export function showError(errorMessage) {
+  return {
+    type: SHOW_ERROR,
+    errorMessage: errorMessage,
+  }
+}
+
+export function loadFrequency(freq)
+//THUNKS
+export function getCosts() {
+  return (dispatch) => {
+    return request
+      .get('/api/v1/costs/')
+      .then((res) => {
+        dispatch(recieveCosts(res.body))
+        return null
+      })
+      .catch((err) => {
+        dispatch(showError(err.message))
+      })
+  }
+}
+
