5ab40242536c4f7959a663a59e61c3e8f0000165
muer-zhang <98420889+muer-zhang@users.noreply.github.com>
2022-05-05 15:13:55 +1200

    Nav bar se up

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Nav () {
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+ const handleLogIn = () => {
+    setLoggedIn(true)
+  }
+
   return (
     <>
- <div>Login</div>
- <div>Register</div> 
- <div>Logout</div> 
+    <h1>Money or Life</h1>
+    
+ <div>
+{loggedIn ? 
+(<div>Logout</div>) 
+: (
+  <>
+<div onClick={handleLogIn}>Login</div>
+<div>Register</div> 
+  </>
+  )}
+   </div>
+ 
  {/* //We will only ever display a max of 2 of these at a time. If user not logged in, we show Login and Register. If they are logged in, we show Logout. */}
     
     </>
