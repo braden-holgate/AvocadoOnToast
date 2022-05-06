60b66c1cc11f8360f453417a7a79d3be59392864
braden-holgate <bradenholgate@live.com>
2022-05-06 16:25:32 +1200

    retirement age calculates and displays correctly

--- client/components/YearsToRetire.jsx
+++ client/components/YearsToRetire.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
+const util = require('../moneyCalcs/timeToRetirement')
 
 function YearsToRetire () {
-  // const financials = const state = useSelector(state => state.state)
+  const [years, setYears] = useState()
+  const financials = useSelector(state => state.financials)
+  const {income, incomePeriod, savings, savingsPeriod} = financials
+
+  console.log(financials)
+  // console.log(util.yearsToRetirement(income, incomePeriod, savings, savingsPeriod))
+
+  useEffect(() => {
+      let tempYears = util.yearsToRetirement(income, incomePeriod, savings, savingsPeriod)
+      setYears(tempYears)
+  }, [financials])
+
   return (
     <>
       <section className="section has-background-light has-text-centered is-size-3 ">
         <h2>Years to retire:</h2>
-        <p id="years">90</p>
+        <p id="years">{years}</p>
       </section>
     </>
   )
