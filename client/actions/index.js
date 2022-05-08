1851aa36a5dd451865948277e721605bcdcce088
Lou <41090489+lou-thomas@users.noreply.github.com>
2022-05-09 11:23:05 +1200

    starting on client side actions. commented out work for now

--- client/actions/index.js
+++ client/actions/index.js
@@ -4,8 +4,13 @@ export const RECEIVE_COSTS = 'RECEIVE_COSTS'
 export const UPDATE_FREQUENCY = 'UPDATE_FREQUENCY'
 export const LOAD_FINANCIALS = 'LOAD_FINANCIALS'
 export const SHOW_ERROR = 'SHOW_ERROR'
+// export const GET_GOALS = 'GET_GOALS'
+// export const UPDATE_GOAL = 'UPDATE_GOAL'
+// export const REMOVE_GOAL = 'REMOVE_GOAL'
+// export const ADD_GOAL = 'ADD_GOAL'
 
 // ACTION CREATORS
+//COSTS & FINANCIALS
 export function recieveCosts(costs) {
   return {
     type: RECEIVE_COSTS,
@@ -34,6 +39,22 @@ export function loadFinancials(financials) {
   }
 }
 
+//GOALS
+
+// export function saveAllGoals() {
+//   return {
+//     type: GET_GOALS,
+//     goals,
+//   }
+// }
+
+// export function updateGoal() {
+//   return {
+//     type: UPDATE_GOAL,
+//     goals,
+//   }
+// }
+
 // THUNKS
 
 export function getCosts() {
@@ -49,3 +70,17 @@ export function getCosts() {
       })
   }
 }
+
+// export function getGoals() {
+//   return (dispatch) => {
+//     return request
+//       .get('/api/v1/goals/')
+//       .then((res) => {
+//         dispatch(saveAllGoals(res.body))
+//         return null
+//       })
+//       .catch((err) => {
+//         dispatch(showError(err.message))
+//       })
+//   }
+// }
