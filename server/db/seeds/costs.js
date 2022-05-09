f425a45fa210a0af487e9d87a8a928638dcfe0a9
braden-holgate <bradenholgate@live.com>
2022-05-09 12:22:30 +1200

    Change item names in seeds to be normal language, update Questions to use global state to populate local state

--- server/db/seeds/costs.js
+++ server/db/seeds/costs.js
@@ -4,8 +4,8 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('Costs').insert([
-        { id: 1, item: 'coffee', cost: 5.00 },
-        { id: 2, item: 'eatingOut', cost: 20.00 }
+        { id: 1, item: 'Coffee', cost: 5.00 },
+        { id: 2, item: 'Eating Out', cost: 20.00 }
       ])
     })
 }
