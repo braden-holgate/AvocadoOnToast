8df54b2f41e436f8e7a75b19824f9a75be1f274f
jessieguo <liondandelion@hotmail.com>
2022-05-10 12:03:44 +1200

    styling

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -19,18 +19,19 @@ function Nav () {
                 <figure className="image columns is-flex">
                   <img className="logo" src="images/logo.png" />
                 </figure>
-                <p className="app-name is-size-3">Avocado.onToast?</p>
+                <p className="app-name">Avocado.onToast ?</p>
               </a>
             <div className="navbar-item">Avo Community Posts</div>
             <div className='navbar-end'> 
               {loggedIn
-                ? (<div className="navbar-item button">Logout</div>)
+                ? (<div className="navbar-item logout-button">Logout</div>)
                 : (
                   <>
                     <div>
                       <div className="navbar-item">
-                        <div className="button"onClick={handleLogIn}>Login</div>
-                        <div className="button">Register</div>
+                        <button className="login-button"onClick={handleLogIn}>Login</button>
+                        <button className="register-button">Register</button>
+                        
                       </div>
                     </div>
                   </>
