e1e9a7790a19716992920f3835c701490b8a04b2
Kate Blincoe <95556689+kate-blincoe@users.noreply.github.com>
2022-05-11 18:32:41 +1200

    add table description to YearsAndTable component

--- client/components/YearsAndTable.jsx
+++ client/components/YearsAndTable.jsx
@@ -16,17 +16,46 @@ function YearsAndTable() {
 
     <>
       {notNull &&
-        <section className='section columns'>
-          <div className="table-left card column">
-            <YearsToRetire />
-            <TimeCostTable />
-          </div>
+        <>
+          <section className='section columns'>
+            <div className="column">
+              <div className="table-description-container table-left card">
+                <div className="content has-text-centered is-size-4">
+                  Based on your current spending habits...
+                </div>
+              </div>
 
-          <div className="table-right card column">
-            <YearsToRetireCompare />
-            <TimeCostTableCompare />
-          </div>
-        </section>
+
+              <div className="arrow-container section has-text-centered">
+                <p className="arrow-left">🠗</p>
+              </div>
+
+              <div className="table-left card">
+                <YearsToRetire />
+                <TimeCostTable />
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="table-description-container table-right card">
+                <div className="content has-text-centered is-size-4">
+                  See how much difference a small change can make!
+                </div>
+              </div>
+
+
+              <div className="arrow-container section has-text-centered">
+                <p className="arrow-right">🠗</p>
+              </div>
+
+              <div className="table-right card">
+                <YearsToRetireCompare />
+                <TimeCostTableCompare />
+              </div>
+            </div>
+
+          </section>
+        </>
       }
     </>
   )
